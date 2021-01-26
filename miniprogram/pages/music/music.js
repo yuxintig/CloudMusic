c8e9441f712d07b2c69313cc11210066432b46fa
Shinonon <1289037748@qq.com>
2021-01-26 17:04:35 +0800

    完成轮播功能，歌单组件

--- miniprogram/pages/music/music.js
+++ miniprogram/pages/music/music.js
@@ -5,6 +5,77 @@ Page({
    * 页面的初始数据
    */
   data: {
+    imgUrls:[{
+      url: 'http://p1.music.126.net/nVUH7O5ZNMG1OQ1kE-tq9g==/109951165665595417.jpg?imageView&quality=89'
+    },
+    {
+      url: 'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+    },
+    {
+      url: 'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+    },
+    {
+      url: 'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+    },
+    {
+      url: 'http://p1.music.126.net/SLfispSeeEnb6Ezs0cNjBw==/109951165666128356.jpg?imageView&quality=89'
+    },
+    {
+      url: 'http://p1.music.126.net/UdSM2BmqY_h_t9HAOzb5dQ==/109951165664710664.jpg?imageView&quality=89'
+    },
+    {
+      url: 'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'
+    },
+    {
+      url: 'http://p1.music.126.net/vAjwukVm-H0LOqzy4FTJXA==/109951165664851877.jpg?imageView&quality=89'
+    },
+    {
+      url: 'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'
+    },
+    {
+      url: 'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'
+    }
+  ],
+  playlist:[{
+    "id":"1001",
+    "playCount":1.4641238e+06,
+    "name":"【翻/原】温柔不是我说 而是你觉得.",
+    "picUrl":"http://p1.music.126.net/PJylNWy_2-jI7LRgQ2Cm6w==/109951165649129522.jpg?param=140y140"
+  },
+  {
+    "id":"1002",
+    "playCount":0.4641238e+06,
+    "name":"随情节流淌|富有叙事感的柔软音乐片段",
+    "picUrl":"http://p2.music.126.net/1hzImIzOsRzG0iBSvHOO7w==/109951165511341127.jpg?param=140y140"
+  },
+  {
+    "id":"1003",
+    "playCount":0.1238e+06,
+    "name":"Children of the City",
+    "picUrl":"http://p1.music.126.net/ySHtkTD4VuhQ3JDv4sreag==/109951165650282126.jpg?param=140y140"
+  },
+  {
+    "id":"1004",
+    "playCount":0.638e+06,
+    "name":"2021·心里装着鲜花银河星光和我爱的人",
+    "picUrl":"http://p2.music.126.net/O8LkkfC7PtV7TA4UP693XA==/109951164569667332.jpg?param=140y140"
+  },
+  {
+    "id":"1005",
+    "playCount":1.9641238e+06,
+    "name":"Acoustic/沉浸柔情海洋",
+    "picUrl":"http://p4.music.126.net/6usq3fdV3_Ldu_3dwqz9yQ==/109951165595569687.jpg?param=140y140"
+  },
+  {
+    "id":"1006",
+    "playCount":0.9641238e+06,
+    "name":"纯音乐｜温柔治愈",
+    "picUrl":"http://p4.music.126.net/vX1HQOKObwAdRMIBCucGQA==/109951165652846742.jpg?param=140y140"
+  }
+  
+
+]
+
 
   },
 
@@ -64,13 +135,3 @@ Page({
 
   }
 })
-
-var pageData = {}
-for (var i = 1; i < 5; ++i) {
-  (function (index) {
-    pageData[`slider${index}change`] = function (e) {
-      console.log(`slider${index}发生change事件，携带值为`, e.detail.value)
-    }
-  })(i);
-}
-Page(pageData)
