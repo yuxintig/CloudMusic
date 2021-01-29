262b7a1046b9abe2991623f3da0490aed2b1addb
Shinonon <1289037748@qq.com>
2021-01-29 21:51:07 +0800

    播放页面布局

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -31,5 +31,10 @@ exports.main = async (event, context) => {
     console.log('######' + res)
     ctx.body = res.data
   })
+
+  app.router('musicUrl', async(ctx, next)=>{
+    const res = await axios.get(`${BASE_URL}/song/url?id=${event.musicId}`)
+    ctx.body = res.data
+  })
   return app.serve()
 }
--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,20 +1,44 @@
 // miniprogram/pages/player/player.js
+let musiclist =[]
+let playingIndex = 0
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    picUrl:''
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     console.log(options)
+    console.log(options.musicId, typeof (options.musicId))
+    playingIndex = options.index
+    musiclist = wx.getStorageSync('musiclist')
+   this._loadMusicDetail(options.musicId)
   },
-
+  _loadMusicDetail(musicId){
+    let music = musiclist[playingIndex]
+    console.log(music)
+    wx.setNavigationBarTitle({
+      title: music.name,
+    })
+    this.setData({
+      picUrl : music.al.picUrl
+    })
+    wx.cloud.callFunction({
+      name:'music',
+      data:{
+        musicId,
+        $url:'musicUrl',
+      
+      }
+    }).then((res) =>{
+      console.log(res)
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
